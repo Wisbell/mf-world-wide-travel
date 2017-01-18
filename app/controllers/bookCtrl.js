dcc21fd19fb47424e01e8f22505fff890e757c7f
Wisbell <westleyisbell@gmail.com>
2017-01-18 15:15:54 -0600

    Moved http request from controller to factory.  Now works properly

--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -1,13 +1,20 @@
 console.log("bookCtrl.js loaded")
 
 
-app.controller('BookCtrl', function($scope, $http){
+app.controller('BookCtrl', function($scope, GuideFactory){
   console.log('BookCtrl controller')
 
-  $http
-    .get('data/guides.json')
-      .then(function(val){
-        $scope.bookList = val.data.guides
-        console.log($scope.bookList)
-      })
+  // $http
+  //   .get('data/guides.json')
+  //     .then(function(val){
+  //       $scope.bookList = val.data.guides
+  //       console.log($scope.bookList)
+  //     })
+
+  GuideFactory.getGuideList()
+    .then(function(val){
+      console.log("val from getGuidesList func", val)
+      $scope.bookList = val
+    })
+
 })
